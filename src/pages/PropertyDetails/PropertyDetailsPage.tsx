11121db196efb70059a4ad9a06c7919ec103448f
chumber33 <123359649+chumber33@users.noreply.github.com>
2023-06-18 11:40:05 +0100

    Added structure to Property Details page
    Added gallery and property details components. Made changes to Property Details page

--- src/pages/PropertyDetails/PropertyDetailsPage.tsx
+++ src/pages/PropertyDetails/PropertyDetailsPage.tsx
@@ -1,11 +1,37 @@
 import React from 'react'
-import ApplyButton from './ApplyButton'
+import styles from '../../App.module.css';
+import Gallary from '../../components/Gallary/Gallary';
+import PropertyDetails from '../../components/PropertyDetails/PropertyDetails';
+import Header from '../../components/Header';
 
 function PropertyDetailsPage() {
+
+  const images = [
+    '/images/image_01.jpg',
+    '/images/image_02.jpg',
+    '/images/image_03.jpg',
+    '/images/image_04.jpg',
+    '/images/image_05.jpg',
+  ];
   return (
+    <React.Fragment>
+
     <div>
-        <ApplyButton />
+      <Header/>
+      <section className={styles['property-details']}>
+        <Gallary images={images} />
+        <PropertyDetails
+          id={"j938hgj"}
+          title="Property Title"
+          price={925}
+          description="Brief Description: Here is a nice property. You will love it!"
+        />
+      </section>
     </div>
+
+    Further Description here?
+
+    </React.Fragment>
   )
 }
 
