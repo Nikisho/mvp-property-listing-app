1b887e19a8d05ce60df743b6b0b46f3bfbd32fa0
Nicholas <phobereromain@gmail.com>
2023-08-02 21:35:46 +0100

    initialised google places autocomplete

--- src/pages/PostListing/AddressForm.tsx
+++ src/pages/PostListing/AddressForm.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import GooglePlacesAutocomplete from 'react-google-places-autocomplete';
 interface FormData {
     address: string;
 };
@@ -7,7 +8,6 @@ interface AddressFormProps extends FormData {
     updateFields: (fields: Partial<FormData>) => void;
 }
 const AddressForm: React.FC<AddressFormProps> = ({ address, updateFields }) => {
-
     return (
         <>
             <div className='flex justify-center'>
@@ -15,11 +15,18 @@ const AddressForm: React.FC<AddressFormProps> = ({ address, updateFields }) => {
             </div>
             <div className='flex flex-col h-full justify-center items-center text-lg font-bold'>
                 <div className='flex space-x-3 w-1/2 justify-center'>
-                    <input className='p-3 rounded-xl border shadow-lg'
+                    {/* <input className='p-3 rounded-xl border shadow-lg'
                         value={address}
                         onChange={e => updateFields({ address: e.target.value })}
                         required={true}
-                    />
+                    /> */}
+                    <div className='p-3 rounded-xl border shadow-lg w-full text-sm'>
+
+                        <GooglePlacesAutocomplete
+                            apiKey={import.meta.env.VITE_GOOGLE_MAPS_APIKEY}
+                            apiOptions={{ language: 'en', region: 'UK' }}
+                        />
+                    </div>
                 </div>
             </div>
         </>
