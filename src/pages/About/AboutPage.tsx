f9ccd2eb980936842515af662f1d9f8c0be72618
Nicholas <phobereromain@gmail.com>
2023-12-13 21:43:40 +0000

    changed about

--- src/pages/About/AboutPage.tsx
+++ src/pages/About/AboutPage.tsx
@@ -10,9 +10,10 @@ const AboutPage = () => {
 				<div className=' space-y-4 border w-full md:w-full lg:w-1/2 xl:w-1/2 p-3 shadow-lg rounded-xl '>
 					<div className='text-xl font-semibold'>What is it?</div>
 					<p className=''>
-						I've been renting for over 7 years and I believe that there should be more transparency between tenants and landlords.
-						Many people are tired of signing leases without knowing what they're getting themselves into. So I thought, "how about a property listing platform where
-						tenants and landlords can review each other?" Ultimately we all want the renting experience more transparent and hassle free.
+						I was a renter for over 5 years and I always felt like there needed to be more transparency between property managers and 
+						tenants. That's when I thought about Propzy, a platform where landlords and tenants can leave reviews. The end goal
+						is to have a data driven platform to help users make better informed decisions when choosing a place to live or a tenant. 
+
 					</p>
 					<div className='text-xl font-semibold'>What stops people from leaving bad reviews without a valid reason?</div>
 					<p className=''>
